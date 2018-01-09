203575aa108f8107e992d4fe77e1e999160503ba
CSMP-FE <csmp@CSMP-FE.(none)>
2018-01-08 19:36:38 -0800

    SNMP TRAP Receiver

--- scripts/external_cmdb.sh
+++ scripts/external_cmdb.sh
@@ -1,7 +1,7 @@
 
 DD=`date +%Y%m%d%H%M%S`
 
-SERVERIP=192.168.1.15
+SERVERIP=192.168.182.20
 OUTFILE=/tmp/cmdb.json.${DD}
 
 authkey=`curl --silent -X POST  http://${SERVERIP}:8080/api/login -d "username=admin&password=password"  | jq -r -S '.authKey'`
