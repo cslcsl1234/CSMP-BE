7e96ce5cd8d6db449cdea835c6cce2e922981e20
guozb <guozb@msn.com>
2018-04-14 10:13:18 +0800

    update the script for privide topo data to CMDB System in AirChina.

--- scripts/external_cmdb.sh
+++ scripts/external_cmdb.sh
@@ -1,9 +1,32 @@
+#
+# the privide the topo data to CMDB For AirChina 
+#
 
 DD=`date +%Y%m%d%H%M%S`
+OUTPATH=/home/ftpuser
+OUTFILE=${OUTPATH}/cmdb/cmdb.json
+TMPFILE=${OUTPATH}/tmp/cmdb.json
 
-SERVERIP=csmpserver
-OUTFILE=/tmp/cmdb.json.${DD}
+authkey=`curl --silent -X POST http://10.2.2.105:8080/api/login -d "username=admin&password=password" | jq -r -S '.authKey'`
+sleep 5
+authkey=`curl --silent -X POST http://10.2.2.105:8080/api/login -d "username=admin&password=password" | jq -r -S '.authKey'`
+echo $authkey
 
-authkey=`curl --silent -X POST  http://${SERVERIP}:8080/api/login -d "username=admin&password=password"  | jq -r -S '.authKey'`
+curl --silent -X GET http://10.2.2.105:8080/api/external/cmdb -H "Authorization: ${authkey}" > ${TMPFILE}.${DD}
 
-curl --silent -X GET http://${SERVERIP}:8080/api/external/cmdb -H "Authorization: ${authkey}"  > ${OUTFILE}
+cp ${TMPFILE}.${DD} ${OUTFILE}
+
+
+sftp ftpuser@10.9.214.104 <<!
+cd /arsys/interface/storagewwpn
+bin
+put /home/ftpuser/cmdb/cmdb.json
+exit
+!
+
+sftp ftpuser@10.9.214.105 <<!
+cd /arsys/interface/storagewwpn
+bin
+put /home/ftpuser/cmdb/cmdb.json
+exit
+!
