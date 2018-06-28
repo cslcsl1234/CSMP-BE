f4c964c88cbe5823f23b6c77b6e748348380f903
guozb <guozb@msn.com>
2018-06-28 13:00:14 +0800

    Fix the bug for ZoneAlias match with fabricwwn.

--- controllers/testController.js
+++ controllers/testController.js
@@ -180,191 +180,205 @@ var testController = function (app) {
 
 
     app.get('/api/test1', function (req, res) {
-
- 
-
-           
-        async.waterfall([
-            function(callback) { 
-    
-                var queryString = " PREFIX  srm: <http://ontologies.emc.com/2013/08/srm#> ";
-                queryString = queryString + " PREFIX  filter:<http://ontologies.emc.com/2015/mnr/topology#> ";
-                queryString = queryString + " PREFIX  rdf:<http://www.w3.org/1999/02/22-rdf-syntax-ns#> ";
-                queryString = queryString + " SELECT distinct ?MV ?MVName ?TARGET ?TARGET_ID ?TARGET_WWN ?INIT ?SGName ";
-                queryString = queryString + " WHERE {  ";
-                queryString = queryString + "     ?MV rdf:type srm:MaskingView .   "; 
-                queryString = queryString + "     ?MV srm:Identifier ?MVID  .  ";
-            
-                queryString = queryString + "     ?MV srm:maskedToStorageVolumeGroup ?SG  .  "; 
-                queryString = queryString + "     ?SG srm:displayName ?SGName  .  "; 
-
-                
-                queryString = queryString + "     ?MV srm:maskedToInitiator ?INIT  .  ";
-                queryString = queryString + "     ?MV srm:maskedToTarget ?TARGET  .  ";
-                queryString = queryString + "     ?TARGET srm:Identifier ?TARGET_ID  .  ";
-                queryString = queryString + "     ?TARGET srm:containsProtocolEndpoint ?TARGET_WWN  .  ";
-                queryString = queryString + "     ?MV srm:viewName ?MVName  .  "; 
-             
-                queryString = queryString + "  }  ";
-    
-
-                var viewResult = [];
-                topos.querySparql(queryString, function(result) {
-                    
-                    for ( var i in result ) {
-                        var item = result[i]; 
-                        var viewItem = {};
-
-                        viewItem["sgname"] = item.SGName; 
-                        viewItem["initgrp"] = item.INIT.split(':')[2];
-                        viewItem["portgrp"] = (item.TARGET.split(':')[3]).replace("~20","");
-                        viewItem["part"] = item.MVName;
-                        viewItem["device"] = item.MV.split(':')[2];
-                        var initgrp_member = [];
-                        initgrp_member.push( item.INIT.split(':')[2] );
-
- 
-
-                        var portgrp_member = [];
-                        var portgrp_member_item={}; 
-                        portgrp_member_item["device"] = item.TARGET_ID.split(':')[2];
-                        portgrp_member_item["feport"] = (item.TARGET_ID.split(':')[3]).replace("~20","")+":"+item.TARGET_ID.split(':')[4];
-                        portgrp_member_item["portwwn"] = item.TARGET_WWN.split(':')[2];
-                        portgrp_member.push(portgrp_member_item);
-
-                        viewItem["initgrp_member"] = initgrp_member;
-                        viewItem["portgrp_member"] = portgrp_member;
-                        
-                        viewResult.push(viewItem);
-                        //console.log(viewItem);
-                    } 
-                    callback(null,viewResult);
-                })
+        res.setTimeout(1200*1000);
+        process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
         
-            },
- 
-            function(arg, callback) {  
- 
-                var param={};
-                param['filter'] = '!parttype&(source==\'VNXBlock-Collector\'|source==\'VNXUnity-Collector\')';
-                param['filter_name'] = '(name=\'UsedCapacity\'|name=\'ConfiguredUsableCapacity\')';
-                param['keys'] = ['serialnb'];
-                param['fields'] = ['device','devdesc','model']; 
-    
-    
-                CallGet.CallGet(param, function(param) {
-                    var arrayinfo = {};
-                    for ( var i in param.result ) {
-                        var item = param.result[i];
+        var fabwwn;
+        async.waterfall(
+            [
+        
+                function(callback){
+                    console.log("TEST1");
 
-                        if ( arrayinfo[item.device] === undefined ) arrayinfo[item.device] = item;
-
-                    }
-                    for ( var j in arg ) {
-                        var item = arg[j];
-                        item["array_model"] = arrayinfo[item.device].model;
-                        item["sn"] = arrayinfo[item.device].serialnb;
-                    }
-                    callback(null,arg);
-                });
-                
-    
-            }, 
-
-            function(arg, callback) { 
-                    
-                
-                    var filterbase = 'datagrp=\'VNXBlock-LUN\'&!vstatus==\'inactive\'';
-                    
-                    if ( start === undefined ) var start = util.getConfStartTime();
-                    if ( end   === undefined ) var end = util.getPerfEndTime();
-            
-            
-                    var param = {}; 
-                    param['period'] = '3600';
-                    param['start'] = start;
-                    param['end'] = end;
-                    param['type'] = 'max';
-                    param['limit'] = 10000000;
-                    param['keys'] = ['serialnb,part']; 
-                    param['fields'] = ['device','partdesc','partsn','partid','sgname'];   
-                    param['filter'] = filterbase;
-                    param['filter_name'] = '(name=\'AssignableCapacity\')';
-                    
-            
-                    CallGet.CallGetPerformance(param, function(ret) {  
-                        var result1 = {};
-                        var resultCapacity = {};
-                        for ( var i in ret ) { 
-                            var item = ret[i];
-                            delete item.matrics;
-                            item["Capacity"] = item.matricsStat.AssignableCapacity.max;
-                            delete item.matricsStat;
-            
-                            // ------------
-                            var device = item.device;
-                            var serialnb = item.serialnb;
-                            var vol = item.partid; 
-            
-                            if ( result1[device] === undefined ) result1[device] = {};
-                            if ( resultCapacity[device] === undefined ) resultCapacity[device] = {};
-                        
-                            var SGNames = item.sgname.split('|'); 
-                            
-                            for ( var j in SGNames ) {
-                                var SGName = SGNames[j];
-                                if ( result1[device][SGName] === undefined ) 
-                                    result1[device][SGName] = [];
-
-
-                                if ( resultCapacity[device][SGName] === undefined ) 
-                                    resultCapacity[device][SGName] = item.Capacity;
-                                else 
-                                    resultCapacity[device][SGName] += item.Capacity;
-
-
-
-                                var sgItem = {};
-                                sgItem["device"] = device;
-                                sgItem["serialnb"] = serialnb;
-                                sgItem["part"] = vol;
-                                sgItem["sgname"] = SGName;
-                                sgItem["Capacity"] = item.Capacity;
-                                sgItem["lunwwn"] = item.partsn;
-                                sgItem["lunname"] = item.partdesc;
-                
-                
-                                result1[device][SGName].push(sgItem);
-
-                            }
-
-                        }
-
-                        for ( var j in arg ) {
-                            var item = arg[j];
-
-                            if ( result1[item.device] !== undefined )
-                                if ( result1[item.device][item.sgname] !== undefined ) { 
-                                     
-                                     item["sg_member"] = result1[item.device][item.sgname];
-                                     item["Capacity"] = resultCapacity[item.device][item.sgname];
-
-                                }
-                                else {
-                                    item["sg_member"] = [];
-                                    item["Capacity"] = 0;
-                                } 
-                        }
-                        callback(null,arg); 
+                    var deviceid;
+                    SWITCH.GetSwitchPorts(deviceid, function(result) {  
+                        callback(null,result); 
                     });
-            }
+                      
+                },
+        
+                // Get All Localtion Records
+                function(param,  callback){ 
+                    console.log("TEST2");
+                    var zoneResult = [];
+                    SWITCH.getFabric(fabwwn,function(resultJson) {
+                        console.log("TEST2.1");
+                        for ( var i in resultJson ) {
+                            var item = resultJson[i];
+                            var zoneItem = {};
+                            var zoneMemberItem = {};
+                            zoneMemberItem['zmemid'] = item.zmemid;
+                            zoneMemberItem['zmemtype'] = item.zmemtype; 
+        
+                            // Search connected to the switch and switch port 
+                            for ( var j in param ) {
+                                var swport = param[j];
+                                //console.log(swport.connectedToWWN +'\t' + item.zmemid);
+                                switch (item.zmemtype) {
+                                    case "Switch Port ID":
+                                        if ( swport.fabwwn == item.pswwn && (swport.domainid+':'+swport.partid) == item.zmemid ) {
+                                            zoneMemberItem['switch'] = swport.device;
+                                            zoneMemberItem['switchport'] = swport.part; 
+                                            zoneMemberItem['switchportwwn'] = swport.partwwn; 
+                                            zoneMemberItem['switchportstate'] = swport.partstat; 
+                                            zoneMemberItem['switchportConnectedWWN'] = swport.portwwn; 
+                                              
+                                            break;  
+                                        }
+                                        break;
+                                    case "Permanent Address":
+                                        if ( swport.connectedToWWN == item.zmemid ) {
+                                            zoneMemberItem['switch'] = swport.device;
+                                            zoneMemberItem['switchport'] = swport.part; 
+                                            zoneMemberItem['switchportwwn'] = swport.partwwn; 
+                                            zoneMemberItem['switchportstate'] = swport.partstat; 
+                                            zoneMemberItem['switchportConnectedWWN'] = swport.portwwn; 
+          
+                                            break;  
+                                        }                            
+                                        break;
+                                }
+        
+                            }
+        
+                            zoneItem["fabricwwn"] = item.pswwn;
+                            zoneItem['device'] = item.device;
+                            zoneItem['zsetname'] = item.zsetname;
+                            zoneItem['zname'] = item.zname;
+                            zoneItem['zonemembers'] = [];
+                            zoneItem.zonemembers.push(zoneMemberItem);
+        
+                            if ( zoneResult.length == 0 ) {
+                                zoneResult.push(zoneItem);
+                            } else {
+                                var isFind = false;
+                                for ( var j in zoneResult) {
+                                    var item1 = zoneResult[j];
+                                    if ( item1.device == item.device &&  
+                                        item1.zsetname == item.zsetname && 
+                                        item1.zname == item.zname 
+                                        ) {
+                                        item1.zonemembers.push(zoneMemberItem);
+                                        isFind = true;
+                                    }
+                                }
+                                if ( ! isFind ) {
+                                    zoneResult.push(zoneItem);
+                                }
+                            }
+        
+                        }
+        
+                       console.log('The number of Zones = ' + zoneResult.length);
+                       callback(null,zoneResult);
+        
+                    })
+        
+                },
+                function(zoneResult, callback){
+                    console.log("TEST3");
+                    var fields = 'part,psname,device,lsname,pswwn';
+                    if ( fabwwn !== undefined )
+                        var filter = 'pswwn=\''+fabwwn+'\'&parttype==\'Fabric\'|parttype==\'VSAN\'';
+                    else 
+                        var filter = 'parttype==\'Fabric\'|parttype==\'VSAN\'';
+                    
+                    
+                    unirest.get(config.Backend.URL + config.SRM_RESTAPI.METRICS_PROPERTIES_VALUE)
+                            .auth(config.Backend.USER, config.Backend.PASSWORD, true)
+                            .headers({'Content-Type': 'multipart/form-data'}) 
+                            .query({'fields': fields , 'filter':  filter }) 
+                            .end(function (response) {
+        
+                                var resultJson = JSON.parse(response.raw_body).values; 
+        
+                                for ( var i in zoneResult ) {
+                                    var item = zoneResult[i];
+                                    for ( var j in resultJson) {
+                                        var fabricItem = resultJson[j];
+                                        if ( fabricItem.pswwn == item.fabricwwn ) {
+                                            item["fabricname"] = fabricItem.psname;
+                                            break;
+                                        }
+                                    }
+                                    
+        
+                                    var zonemembers = item.zonemembers;
+                                    for ( var j in zonemembers ) {
+                                        var zoneitem = zonemembers[j];
+                                        for ( var z in resultJson ) {
+                                            var switem = resultJson[z];
+                                            if ( zoneitem.switch == switem.device ) {
+                                                var switchid = zoneitem.switch;
+                                                zoneitem["switch"] = switem.lsname;
+                                                zoneitem["switch_oriname"] = switchid;
+                                            }
+                                        }
+                                        
+                                    }
+        
+                                }
+        
+                                callback(null,zoneResult);
+                            });
+                      
+                } ,               
+                function(arg1,  callback){ 
+                    res.json
+                    console.log("TEST4");
+                    
+                    var param = {}; 
+                    param['filter'] = 'parttype==\'ZoneAlias\'';
+                    param['fields'] = ['pswwn','alias','zmemid'];
+                    param['keys'] = ['pswwn','alias','zmemid'];
+        
+                    CallGet.CallGet(param, function(param) { 
+                        for ( var i in arg1 ) {
+                            var zoneitem = arg1[i];
+                            
+        
+                            for ( var z in zoneitem.zonemembers ) {
+                                var item = zoneitem.zonemembers[z];
+                                item['alias'] = '';
+                                if ( item.zmemid != '' ) {
+        
+                                    for ( var j in param.result ) {
+                                        var aliasItem = param.result[j];
+        
+                                        if ( zoneitem.fabricwwn == aliasItem.pswwn & item.zmemid == aliasItem.zmemid ) {
+                                            if ( item.alias == '' )
+                                                item['alias'] = aliasItem.alias;
+                                            else 
+                                                item['alias'] = item.alias + ',' + aliasItem.alias;
+                                        }
+                                    }
+        
+                                }
+                            }
+                        }
+                        callback(null,arg1);
+                    });                 
+                } 
+            ], function (err, result) {
+                  // result now equals 'done'
+                  res.json(200,result);
+            });       
+
+     });     
+     
+     app.get('/test2',function(req, res) {
+                            
+        var param = {}; 
+        param['filter'] = 'parttype==\'ZoneAlias\'';
+        param['fields'] = ['pswwn','alias','zmemid'];
+        param['keys'] = ['pswwn','alias','zmemid'];
+
+        CallGet.CallGet(param, function(param) { 
+
             
-        ], function (err, result) {
-
-            res.json(200,result);
-         });
-
-     });                       
+            res.json(200,param.result);
+        });                
+     })
 
     app.get('/api/test', function (req, res) {
         res.setTimeout(1200*1000);
