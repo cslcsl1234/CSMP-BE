141567c9ccf3b5772a21f1631d15c080afe5581b
guozb <guozb@msn.com>
2018-07-17 16:17:14 +0800

    Update a bug for backendmgmt and update 2 scripts for ceb_topo.sh&dailyTask.sh

---
+++ scripts/dailyTask.sh
@@ -0,0 +1,18 @@
+echo " --------------------- begin ---------------------------------"
+DD=`date +%Y%m%d%H%M%S`
+YESTODAY_DD=`date -d yesterday +%Y%m%d`
+
+authkey=`curl --silent -X POST http://csmpserver:8080/api/login -d "username=admin&password=password" | jq -r -S '.authKey'`
+sleep 5
+authkey=`curl --silent -X POST http://csmpserver:8080/api/login -d "username=admin&password=password" | jq -r -S '.authKey'`
+
+echo "authkey="$authkey
+
+
+curl --silent -X GET http://csmpserver:8080/api/backendmgmt/monitoring/serverstatus?execute=true -H "Authorization: ${authkey}"
+
+curl --silent -X GET http://csmpserver:8080/api/backendmgmt/monitoring/testvaild -H "Authorization: ${authkey}"
+curl --silent -X GET http://csmpserver:8080/api/backendmgmt/monitoring/mgmtobjects -H "Authorization: ${authkey}"
+
+
+echo " --------------------- end ---------------------------------"
