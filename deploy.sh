c10c88a0096ddc9b767767bb796caaefee4231d2
root <root@instance-5.us-central1-a.c.plexiform-zone-238714.internal>
2019-07-10 19:43:26 +0000

    update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -5,13 +5,13 @@ wget https://nodejs.org/dist/v10.14.1/node-v10.14.1-linux-x64.tar.gz
 tar -zxvf node-v10.14.1-linux-x64.tar.gz -C /opt
 
 echo "export PATH=$PATH:/opt/node-v10.14.1-linux-x64/bin" >> /etc/profile
-. /etc/profile
 
 wget http://people.centos.org/tru/devtools-2/devtools-2.repo -O /etc/yum.repos.d/devtools-2.repo
 yum install devtoolset-2-gcc devtoolset-2-binutils devtoolset-2-gcc-c++ -y
 
-git clone https://linux6200:umcewT9a@github.com/linux6200/CSMP-BE.git
+#git clone https://linux6200:umcewT9a@github.com/linux6200/CSMP-BE.git
 cd CSMP-BE
+. /etc/profile
 scl enable devtoolset-2 bash
 
 npm install  --unsafe-perm
