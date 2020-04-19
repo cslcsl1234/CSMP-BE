734ab74d062b2d510bfab98f6bdfef6915797ab5
guozb <guozb@msn.com>
2020-04-19 00:21:50 -0400

    update backup/load data scripts

--- exportmenu.sh
+++ exportmenu.sh
@@ -1,4 +1,4 @@
-mongoexport -d csmp -c menus -o menus.dat
-mongoexport -d csmp -c users -o users.dat
-mongoexport -d csmp -c roles -o roles.dat
+mongoexport -h csmpdbserver -d csmp -c menus -o menus.dat
+mongoexport -h csmpdbserver -d csmp -c users -o users.dat
+mongoexport -h csmpdbserver -d csmp -c roles -o roles.dat
 
--- importmenu.sh
+++ importmenu.sh
@@ -1,4 +1,4 @@
-mongoimport -d csmp -c menus --drop menus.dat
-mongoimport -d csmp -c users --drop users.dat
-mongoimport -d csmp -c roles --drop roles.dat
+mongoimport -h csmpdbserver -d csmp -c menus --drop menus.dat
+mongoimport -h csmpdbserver -d csmp -c users --drop users.dat
+mongoimport -h csmpdbserver -d csmp -c roles --drop roles.dat
 
