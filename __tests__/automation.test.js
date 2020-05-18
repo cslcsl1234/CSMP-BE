218f90acdcdff6d6bd0e3ca607ba9d82674b9c25
zhibin.guo@dell.com <zhibin.guo@dell.com>
2020-05-17 19:38:14 -0700

    automation service update

---
+++ __tests__/automation.test.js
@@ -0,0 +1,48 @@
+
+const AUTO = require('../lib/Automation');
+
+describe("Automation Service", () => {
+
+    var executeParamaters = [
+        {
+            "StepGroupName": "RPA Consistoncy Group Prod journal volume",
+            "Step": "rediscover physical volume",
+            "method": "ReDiscoverPhysicalArray",
+            "arrayinfo": {
+              "name": "EMCCTEST",
+              "type": "VPLEX",
+              "version": "5.5",
+              "endpoint": "https://10.32.32.100/vplex",
+              "auth": {
+                "username": "service",
+                "password": "password"
+              }
+            }
+          },
+          {
+            "StepGroupName": "RPA Consistoncy Group Prod journal volume",
+            "Step": "claim the pyhsical volume",
+            "method": "ClaimPhysicalVolume",
+            "arrayinfo": {
+              "name": "EMCCTEST",
+              "type": "VPLEX",
+              "version": "5.5",
+              "endpoint": "https://10.32.32.100/vplex",
+              "auth": {
+                "username": "service",
+                "password": "password"
+              }
+            }
+          }
+    ]
+
+    test("AUTO.ExecuteActions", (done) => {
+        var ws;
+        AUTO.ExecuteActions(executeParamaters, ws, function (result) {
+            console.log(result);
+            done();
+        })
+    },60000)
+
+
+})
